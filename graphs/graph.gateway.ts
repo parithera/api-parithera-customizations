de4ecb75be7fcea4c77df65109253c2c2187e552
CedricHerzog <CedricHerzog@users.noreply.github.com>
2024-11-21 17:05:38 +0100

    Use websocket to fetch graphs

---
+++ graphs/graph.gateway.ts
@@ -0,0 +1,104 @@
+import { UseFilters, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+    BaseWsExceptionFilter,
+    ConnectedSocket,
+    MessageBody,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+    WsException,
+    WsResponse
+} from '@nestjs/websockets';
+import { readFile } from 'fs';
+import { join } from 'path';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Server, Socket } from 'socket.io';
+import { CombinedAuthGuard } from 'src/codeclarity_modules/auth/guards/combined.guard';
+import { OrganizationsMemberService } from 'src/codeclarity_modules/organizations/organizationMember.service';
+import { AuthUser } from 'src/decorators/UserDecorator';
+import { MemberRole } from 'src/entity/codeclarity/OrganizationMemberships';
+import { Project } from 'src/entity/codeclarity/Project';
+import { Result } from 'src/entity/codeclarity/Result';
+import { AuthenticatedUser } from 'src/types/auth/types';
+import { Repository } from 'typeorm';
+
+interface Request {
+    projectId: string;
+    orgId: string;
+    type: string;
+}
+
+interface Response {
+    data: string;
+    type: string;
+}
+
+@WebSocketGateway({
+    cors: {
+        origin: '*'
+    },
+    inheritAppConfig: true
+})
+@UseGuards(CombinedAuthGuard)
+export class GraphsGateway {
+    constructor(
+        private readonly organizationMemberService: OrganizationsMemberService,
+        @InjectRepository(Result, 'codeclarity')
+        private resultRepository: Repository<Result>,
+        @InjectRepository(Project, 'codeclarity')
+        private projectRepository: Repository<Project>
+    ) {}
+    @WebSocketServer()
+    server: Server;
+
+    @SubscribeMessage('graphs')
+    async graph(
+        @MessageBody() data: Request,
+        @AuthUser() user: AuthenticatedUser
+    ): Promise<Response> {
+        await this.organizationMemberService.hasRequiredRole(
+            data.orgId,
+            user.userId,
+            MemberRole.USER
+        );
+
+        const project = await this.projectRepository.findOne({
+            where: {
+                id: data.projectId,
+                organizations: {
+                    id: data.orgId
+                }
+            },
+            relations: {
+                added_by: true
+            }
+        });
+        if (!project) {
+            throw new Error('Project not found');
+        }
+
+        const filePath = join(
+            '/private',
+            project.added_by.id,
+            data.projectId,
+            `plot_${data.type}.png`
+        );
+        const response: Response = {
+            data: '',
+            type: data.type
+        };
+
+        return new Promise((resolve, reject) => {
+            return readFile(filePath, 'base64', (err, data) => {
+                if (err) {
+                    reject(err);
+                }
+                response.data = data;
+                resolve(response);
+            });
+        });
+    }
+}
--- graphs/graphs.module.ts
+++ graphs/graphs.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrganizationsMemberService } from 'src/codeclarity_modules/organizations/organizationMember.service';
 import { OrganizationMemberships } from 'src/entity/codeclarity/OrganizationMemberships';
 import { Project } from 'src/entity/codeclarity/Project';
+import { GraphsGateway } from './graph.gateway';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Result, Project, OrganizationMemberships], 'codeclarity')],
-    providers: [OrganizationsMemberService],
+    providers: [OrganizationsMemberService, GraphsGateway],
     controllers: [GraphController]
 })
 export class GraphModule {}
